0d75bc2afef7b6afa3611d953b332eb217f8fb3a
davidmorison <david.morison@moneysavingexpert.com>
2018-02-02 16:51:31 +0000

    initial googlesheets testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class App extends Component {
+	getSheetsData() {
+		const key = '2PACX-1vSt81EPodQlmjR3hMJkqB1cr2MFODU8rzemtqAR_OfqIUT2szHemzPOKyziRyuDYt1TYdmND2BoTrmL';
+		const urlJson = `https://spreadsheets.google.com/feeds/list/${key}/od6/public/basic?hl=en_US&alt=json`;
+		const urlCsv = `https://docs.google.com/spreadsheets/d/e/${key}/pub?output=csv`;
+		
+		axios.get(urlJson)
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
   render() {
+  	const myData = this.getSheetsData();
+
     return (
       <div className="App">
         
