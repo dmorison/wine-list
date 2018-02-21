85629c328ee47ca5ff6dfa01bd4dd918cc677b30
David Morison <davemorison@gmail.com>
2018-02-21 07:25:02 +0000

    adding images and body background

--- src/js/components/wine_item.js
+++ src/js/components/wine_item.js
@@ -6,14 +6,16 @@ const WineItem = ({wine, onWineSelect}) => {
 
 	return (
 		<Col className="wine-item" xs={12} sm={6} md={3}>
-			<img src={`./${wine[14]}.png`} width="60" height="60" />
-			<ul>
-				<li>{wine[1]} - {wine[2]}</li>
-				<li>{wine[4]}, {wine[3]}</li>
-				<li>{wine[5]}</li>
-				<li>{wine[6]}</li>
-				<li><a href="#" onClick={() => onWineSelect(wine)}>View details</a></li>
-			</ul>
+			<div className="thumbnail">
+				<img src={process.env.PUBLIC_URL + `/images/wine_thumbnails/${wine[14]}.jpg`} width="60" height="60" />
+				<ul>
+					<li>{wine[1]} - {wine[2]}</li>
+					<li>{wine[4]}, {wine[3]}</li>
+					<li>{wine[5]}</li>
+					<li>{wine[6]}</li>
+					<li><a href="#" onClick={() => onWineSelect(wine)}>View details</a></li>
+				</ul>
+			</div>
 		</Col>
 	);
 
